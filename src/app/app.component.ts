487f6d0680143756bb249efe8fa3c2f75157b00f
ZZDome <101982929+ZZDome@users.noreply.github.com>
2022-10-15 14:16:02 +0200

    add slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'NG_Website';
+export class AppComponent implements OnInit {
+  title = 'my Website';
+  slideImages = ['ph1.png', 'ph2.jpg', 'ph3.png'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.slideshow();
+  }
+
+  slideshow(){
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.slideImages.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1)
+    },8000)
+  }
 }
